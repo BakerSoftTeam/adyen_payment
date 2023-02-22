890e5344fbfc9c381e7bfe7b4fe811277211ea8a
Emil Falkowski <femil90@gmail.com>
2023-02-22 13:15:06 +0100

    feat: change the dio timeout to milliseconds

--- lib/src/service/adyen_terminal_service.dart
+++ lib/src/service/adyen_terminal_service.dart
@@ -81,7 +81,7 @@ Dio _createDefaultDio(PointOfSaleConfig config) {
 }
 
 BaseOptions _buildBaseOptions(String baseUrl) {
-  const Duration timeout = Duration(seconds: 15000);
+  const Duration timeout = Duration(milliseconds: 15000);
   return BaseOptions(
     baseUrl: baseUrl,
     connectTimeout: timeout,
